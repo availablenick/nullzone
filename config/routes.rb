9c8bb7772c7c820b441602faac341d96e7fbfd00
derpless <derpzord@derpzord.localdomain>
2019-05-06 04:05:32 +0000

    add topicos and usuarios routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'main/index', as: :main
+  
+  resources :topicos
+  resources :usuarios
+
+  root to: 'main#index'
 end
