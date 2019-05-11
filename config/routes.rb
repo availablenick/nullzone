88e88f01bc897fb4280f89caf21692433299ff08
derpless <leoap711@gmail.com>
2019-05-11 20:39:40 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'main/index', as: :main
   
+  resources :topicos do
+    resources :respostas
+  end
+
   resources :usuarios do
     resources :respostas
-
-    resources :topicos do
-      resources :respostas
-    end
+    resources :topicos 
   end
 
   root to: 'main#index'
