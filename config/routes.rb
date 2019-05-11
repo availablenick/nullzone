b6b61817c8ebea3bf709158b6af204e823807ef3
derpless <derpzord@derpzord.localdomain>
2019-05-11 19:28:26 +0000

    configure respostas routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 	get 'main/index', as: :main
   
-  resources :topicos
-  resources :usuarios
+  resources :topicos do
+    resources :respostas
+  end
+
+  resources :usuarios do
+    resources :respostas
+  end
 
   root to: 'main#index'
 end
