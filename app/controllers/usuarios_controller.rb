bd6f0b51cd8a52bd64e1f66fe381b1c8e9f6d236
derpless <leoap711@gmail.com>
2019-05-12 01:40:58 +0000

    fix Usuario controller, forbid blank name and password

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,6 +1,18 @@
-class UsuariosController < ApplicationController
+class UsuariosController < ApplicationController  
+  def new
+    @usuario = Usuario.new
+  end
 
-    def new
+  def create
+    @usuario = Usuario.new(usuario_params)
+    
+    if @usuario.save
+      redirect_to main_path
     end
+  end
 
+  private
+    def usuario_params
+      params.require(:usuario).permit(:nome, :senha)
+    end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,6 @@
 class Usuario < ApplicationRecord
   has_many :topicos
   has_many :respostas
+
+  validates :nome, :senha, presence: true
 end
