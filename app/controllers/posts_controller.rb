4d641f66d1ec1350ff0ff6473ea153cf1bec1117
derpless <leoap711@gmail.com>
2019-05-12 06:35:42 +0000

    configure posts controller, adjust post visualization on page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  def create
+    @topico = Topico.find(params[:topico_id])
+    @post = @topico.posts.create(post_params)
+
+    redirect_to topico_path(@topico)
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:mensagem, :topico_id, :usuario_id)
+    end
 end
