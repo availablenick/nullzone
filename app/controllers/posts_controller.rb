5fc6d240fd38365466c72d465969ddebca9f6030
derpless <leoap711@gmail.com>
2019-05-18 01:08:25 +0000

    add edit option to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
+  def edit
+    @topico = Topico.find(params[:topico_id])
+    @post = @topico.posts.find(params[:id])
+  end
+
   def create
     @topico = Topico.find(params[:topico_id])
     @post = @topico.posts.create(post_params)
 
     redirect_to topico_path(@topico)
+  end
+
+  def update
+    @topico = Topico.find(params[:topico_id])
+    @post = @topico.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to topico_path(@topico)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
