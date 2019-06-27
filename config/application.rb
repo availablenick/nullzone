9f961cd1f152e1b0263913f86595a2ef26b9eec1
derpless <leoap711@gmail.com>
2019-06-27 04:23:42 +0000

    Add assets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Nullzone
     config.load_defaults 5.2
     config.time_zone = 'Brasilia'
     config.i18n.default_locale = :pt
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
