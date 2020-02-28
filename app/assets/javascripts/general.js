d5b3a18f58288463c088983c45ec04aefa3d17a8
derpless <leoap711@gmail.com>
2020-02-28 20:22:13 -0300

    Add 'push-to-right' effect for sidebar

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,42 +1,30 @@
 $(document).on('turbolinks:load', function() {
 
-  $sidebarBg = $('.sidebar-bg');
+  $topbar = $('.topbar');
+  $sidebar = $('.sidebar');
+  $mainWrapper = $('.main-wrapper');
+  $mainContent = $('.main-content');
 
   // Topbar behavior
   $(document).on('scroll', function() {
     let bannerHeight = $('.banner').css('height');
     bannerHeight = bannerHeight.slice(0, bannerHeight.length - 2);
     if (window.scrollY > bannerHeight) {
-      $('.topbar').css('position', 'fixed');
-
-      $sidebarBg.css('position', 'fixed');
-      $sidebarBg.css('top', '0');
-      $sidebarBg.css('margin-top', '50px');
-
-      $('.main-wrapper').css('padding-top', '100px');
+      $topbar.css('position', 'fixed');
+      $sidebar.css('position', 'fixed');
+      $sidebar.css('top', '50px');
+      $mainWrapper.css('padding-top', '100px');
     } else {
-      $('.topbar').css('position', 'relative');
-      
-      $sidebarBg.css('position', 'absolute');
-      $sidebarBg.css('top', 'auto');
-      $sidebarBg.css('margin-top', '0');
-      
-      $('.main-wrapper').css('padding-top', '50px');
+      $topbar.css('position', 'relative');
+      $sidebar.css('position', 'absolute');
+      $sidebar.css('top', 'auto');
+      $mainWrapper.css('padding-top', '50px');
     }
   });
 
   // Toggle sidebar
   $toggleBtn = $('.toggle-sidebar');
   $bar = $toggleBtn.find('.bar');
-  $sidebarBg.click(function() {
-    $('.sidebar').animate({ left: '-17%' }, 300, function() {
-      $toggleBtn.removeClass('clicked');
-      $toggleBtn.removeAttr('style');
-      $bar.removeAttr('style');
-
-      $sidebarBg.css('display', 'none');
-    });
-  });
 
   $toggleBtn.click(function() {
     if (!$toggleBtn.hasClass('clicked')) {
@@ -44,15 +32,19 @@ $(document).on('turbolinks:load', function() {
       $toggleBtn.css('background', 'white');
       $bar.css('background', 'black');
       $bar.eq(1).css('margin', '6px 0');
-      $sidebarBg.css('display', 'block');
-      $('.sidebar').animate({ left: '0' }, 300);
+
+      $sidebar.animate({ left: '0' }, { duration: 300, queue: false });
+      $mainContent.animate({ marginLeft: '175px' }, { duration: 300, queue: false });
     } else {
-      $('.sidebar').animate({ left: '-17%' }, 300, function() {
+      $sidebar.animate({ left: '-175px' }, { duration: 300, queue: false, done: function() {
         $toggleBtn.removeClass('clicked');
         $toggleBtn.removeAttr('style');
         $bar.removeAttr('style');
-        $sidebarBg.css('display', 'none');
-      });
+      } });
+
+      $mainContent.animate({ marginLeft: '0' }, { duration: 300, queue: false , done: function() {
+        $(this).removeAttr('style');
+      } });
     }
   });
 });
