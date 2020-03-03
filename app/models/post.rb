317ec84ce68c225397b69c855dcb1a66e58519e8
availablenick <leoap711@gmail.com>
2020-03-03 18:50:23 -0300

    Allow posts/topics with complaints to be deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :topic
 
-  has_many :complaints, as: :complainable
+  has_many :complaints, as: :complainable, dependent: :nullify
   has_many :ratings, dependent: :destroy
 
   validates :message, presence: true
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :user
   belongs_to :section
   
-  has_many :complaints, as: :complainable
+  has_many :complaints, as: :complainable, dependent: :nullify
   has_many :posts, dependent: :destroy
 
   validates :title, :message, presence: true
