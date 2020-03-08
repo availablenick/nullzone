d72052f03bf1f0ef8cfeac85100c4b9c7ade28ff
availablenick <leoap711@gmail.com>
2020-03-08 14:51:41 -0300

    Remove validation of which_type attribute in complaint model

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -2,5 +2,5 @@ class Complaint < ApplicationRecord
   belongs_to :user
   belongs_to :complainable, polymorphic: true
 
-  validates :which_type, :complainee, presence: true
+  validates :complainee, presence: true
 end
