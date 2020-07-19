8435c35e393924a3e1139cd8595db091d8c1971c
availablenick <leoap711@gmail.com>
2020-07-19 19:30:32 -0300

    Update ruby version, fix database's settings and update README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-adm = User.create(login: 'ADM', password: 'senha', password_confirmation: 'senha', signature: 'EU Q MANDO')
+adm = User.create(login: 'ADM', password: 'password', password_confirmation: 'password', signature: 'EU Q MANDO')
 users = []
 users.push(adm)
 (1..20).each do |i|
-  users.push(User.create(login: "User#{i}", password: 'senha', password_confirmation: 'senha'))
+  users.push(User.create(login: "User#{i}", password: 'password', password_confirmation: 'password'))
 end
 
-off_topic = Section.create(name: 'Área geral', description: 'Local [único] para discussão de assuntos diversos.')
-Section.create(name: 'Bueiro', description: 'Miséria completa.')
+off_topic = Section.create(name: 'Section1', description: 'Section1\'s description.')
+Section.create(name: 'Section2', description: 'Section2\'s description.')
 
 (1..10).each do |n|
   Topic.create(title: "#{n}", message: '0', parsed_message: '0', user_id: adm.id, section_id: off_topic.id)
